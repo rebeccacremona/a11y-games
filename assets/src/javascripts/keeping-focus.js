5734f39330ddcf056184d6fed41b6b59561224cc
rcremona <rebecca_cremona@harvard.edu>
2017-07-03 17:38:49 -0400

    Add staircase to keeping-focus challenge.

--- assets/src/javascripts/keeping-focus.js
+++ assets/src/javascripts/keeping-focus.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
+  // Display the staircase and place the user at the top of the stairs
+  document.getElementById('to-the-staircase').addEventListener('click', function(){
+    this.style.display = "none";
+    document.getElementById('candle-wrapper').style.display = "none";
+    document.getElementById('staircase-wrapper').style.display = "block";
+    document.getElementById('staircase-top').focus();
+  })
 
 });
